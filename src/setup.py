26142ad9c88d78a5402c5d46f83d3f14c2af5782
Jason Wee <peichieh@gmail.com>
2015-06-21 17:35:18 +0800

    added setup directive.

---
+++ src/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+def readme():
+    with open('../README.md') as f:
+        return f.read()
+
+setup(name='router_statistics',
+      version='0.1',
+      description='retrieve router statistics and display on graph',
+      long_description=readme(),
+      classifiers=['Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: System :: Networking :: Monitoring'],
+      keywords='asus monitoring graph',
+      url='https://github.com/jasonwee/asus-rt-n14uhp-mrtg',
+      author='Jason Wee',
+      author_email='peichieh@gmail.com',
+      license='Apache version 2.0',
+      packages=['router_statistics'],
+      include_package_data=False,
+      zip_safe=False)
