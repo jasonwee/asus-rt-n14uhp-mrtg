ee28075764af1c4b795c85af476c051592e5cf44
Jason Wee <peichieh@gmail.com>
2015-06-21 18:12:43 +0800

    corrected path

--- src/router_statistics/test/router_stats_test.py
+++ src/router_statistics/test/router_stats_test.py
@@ -11,13 +11,13 @@ class RouterStatsTestCase(unittest.TestCase):
     """Tests for routerStats.py. """
 
     def test_get_cpu_stat_from_router(self):
-        stream = open("../conf/config.yaml", "r")
+        stream = open("../../../conf/config.yaml", "r")
         config = yaml.load(stream)
         self.assertTrue(get_cpu_stat_from_router(config) is not None)
 
 if __name__ == '__main__':
         from os import sys, path
         sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-        from src.routerStats import get_cpu_stat_from_router
+        from routerStats import get_cpu_stat_from_router
         unittest.main()
 
