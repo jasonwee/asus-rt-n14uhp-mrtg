2bbac1f1ad79a613ed79e2458933220ec76d8d4b
Jason Wee <peichieh@gmail.com>
2016-06-14 22:36:55 +0800

    python3.4 environment

---
+++ tmp/ve_asus-rt-n14uhp-mrtg/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jason/oss/asus-rt-n14uhp-mrtg/tmp/ve_asus-rt-n14uhp-mrtg/bin/python3.4
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
