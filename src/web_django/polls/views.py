778fd88e5b7826f8be1faa11b40251c6bf9ba9dc
Jason Wee <peichieh@gmail.com>
2016-06-15 16:15:16 +0800

    change to generic views

--- src/web_django/polls/views.py
+++ src/web_django/polls/views.py
@@ -3,10 +3,23 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.views import generic
+from django.utils import timezone
 from django.core.urlresolvers import reverse
 
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions. (not including those set to
+        be published in the future).
+        
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+"""
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
@@ -15,17 +28,30 @@ def index(request):
     }
     #output = ','.join([q.question_text for q in latest_question_list])
     return HttpResponse(template.render(context, request))
+"""
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+"""
 def detail(request, question_id):
     try:
         question = Question.objects.get(pk=question_id)
     except Question.DoesNotExist:
         raise Http404("Question does not exists")
     return render(request, 'polls/detail.html', {'question':question})
+"""
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+"""
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+"""
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
