87f2a4b968807b8d24dda01e51203f41dbca8e41
Jason Wee <peichieh@gmail.com>
2018-01-14 01:02:05 +0800

    remaining re lesson

---
+++ src/lesson_text/re_paragraphs_findall.py
@@ -0,0 +1,16 @@
+import re
+
+text = '''Paragraph one
+on two lines.
+
+Paragraph two.
+
+
+Paragraph three.'''
+
+for num, para in enumerate(re.findall(r'(.+?)\n{2,}',
+                                      text,
+                                      flags=re.DOTALL)
+                           ):
+    print(num, repr(para))
+    print()
