fff750588cf6042a91d0422a20773a2a548b4b73
Jason Wee <peichieh@gmail.com>
2018-02-11 22:11:46 +0800

    learn queue

---
+++ src/lesson_data_structures/fetch_podcasts.py
@@ -0,0 +1,20 @@
+from queue import Queue
+import threading
+import time
+import urllib
+from urllib.parse import urlparse
+
+import feedparser
+
+# Set up some global variables
+num_fetch_threads = 2
+enclosure_queue = Queue()
+
+# A real app wouldn't use hard-coded data...
+feed_urls = [
+    'http://talkpython.fm/episodes/rss',
+]
+
+
+def message(s):
+    print('{}: {}'.format(threading.current_thread().name, s))
---
+++ src/lesson_data_structures/queue_fifo.py
@@ -0,0 +1,14 @@
+
+
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+    q.put(i)
+
+while not q.empty():
+    print(q.get(), end=' ')
+print()
+
+
