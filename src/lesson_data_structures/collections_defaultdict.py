a02cc07d113728c6d80cd4494da920a4e558c763
Jason Wee <peichieh@gmail.com>
2018-02-11 18:42:16 +0800

    learn defaultdict

---
+++ src/lesson_data_structures/collections_defaultdict.py
@@ -0,0 +1,9 @@
+import collections
+
+def default_factory():
+    return 'default value'
+
+d = collections.defaultdict(default_factory, foo='bar')
+print('d:', d)
+print('foo =>', d['foo'])
+print('bar =>', d['bar'])
