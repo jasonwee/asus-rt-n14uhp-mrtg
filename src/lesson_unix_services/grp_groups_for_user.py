4fa96c3406e32ea6631ce447db6d19d70b2cd061
Jason Wee <peichieh@gmail.com>
2018-10-05 10:16:41 +0800

    learn unix services

---
+++ src/lesson_unix_services/grp_groups_for_user.py
@@ -0,0 +1,9 @@
+import grp
+
+username = 'dhellmann'
+group_names = set(
+    g.gr_name
+    for g in grp.getgrall()
+    if username in g.gr_mem
+)
+print(username, 'belongs to:', ', '.join(sorted(group_names)))
